d1ef9283536b6a8ac74a21a951c0a53514e9b327
Aditya Singh <adityasingh740745@gmail.com>
2024-12-14 22:57:31 +0530

    fix: update tailwind content scanning

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
   content: [
     "./index.html",
+    "./main.js",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
@@ -9,4 +10,3 @@ export default {
   },
   plugins: [],
 }
-
